cd59689042d9a378203bf878982f7e6ca027b726
Enquiet <you@example.com>
2020-09-01 12:11:46 +0300

    added two new modules

---
+++ src/components/ProductList.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul class="catalog__list">
+        <li class="catalog__item" v-for="(productItem,index) in productList" :key="index">
+           <ProductItem :product="productItem"/>
+        </li>
+    </ul>
+</template>
+
+<script>
+import ProductItem from './ProductItem.vue';
+
+export default {
+  props: ['productList'],
+  components: { ProductItem },
+};
+
+</script>
