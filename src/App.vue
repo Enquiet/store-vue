f31163e77f73df034606cce06ad12f51c5aa0d3c
Enquiet <you@example.com>
2020-09-30 20:59:52 +0300

    Добавил страницу товара

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,29 @@
 <template>
-  <main class="content container">
-    <div class="content__top content__top--catalog">
-      <h1 class="content__title">
-        Каталог
-      </h1>
-      <span class="content__info">
-        152 товара
-      </span>
-    </div>
-
-    <div class="content__catalog">
-      <ProductFilter  :price-to.sync="productPriceTo" :price-from.sync="productPriceFrom"  :category-id.sync="productFilterId"
-      :category-color.sync="productFilterColor" :category-memory.sync="productFilterMamory"/>
-      <section class="catalog">
-        <ProductList :productList="goods"/>
-        <BasePagination :page.sync="page" :all-products="getAllProduct" :per-products="NumberProductsPerPage"/>
-      </section>
-    </div>
-  </main>
-
+  <component :is="currentPage"/>
 </template>
 
 <script>
-import goods from './data/goods'
-import ProductList from './components/ProductList.vue'
-import BasePagination from './components/BasePagination.vue'
-import ProductFilter from './components/ProductFilter.vue'
+import MainPage from './page/MainPage.vue'
+import ProductPage from './page/ProductPage.vue'
+import NotFoundPage from './page/NotFoundPage.vue'
+
+const routerPage = {
+  mainPage: 'MainPage',
+  productPage: 'ProductPage'
+}
 
 export default {
   name: 'App',
-  components: { ProductList, BasePagination, ProductFilter },
-
+  components: { MainPage, ProductPage, NotFoundPage },
   data () {
     return {
-      productPriceFrom: 0,
-      productPriceTo: 0,
-      productFilterId: 0,
-      productFilterColor: '',
-      productFilterMamory: [],
-
-      page: 1,
-      NumberProductsPerPage: 4
-
+      page: 'mainPage'
     }
   },
   computed: {
-    getFilterProducts () {
-      let filterProducts = goods
-
-      if (this.productPriceFrom > 0) {
-        filterProducts = filterProducts.filter(product => product.priceGoods > this.productPriceFrom)
-      }
-
-      if (this.productPriceTo > 0) {
-        filterProducts = filterProducts.filter(product => product.priceGoods < this.productPriceTo)
-      }
-      if (this.productFilterId) {
-        filterProducts = filterProducts.filter(product => product.goodsId === this.productFilterId)
-      }
-      if (this.productFilterMamory > 0) {
-        let allProductMemory
-        for (let i = 0; i < this.productFilterMamory.length; i++) {
-          allProductMemory = this.productFilterMamory[i]
-        }
-        filterProducts = filterProducts.filter(product => product.memory && product.memory.every(color => color.value === allProductMemory))
-      }
-      if (this.productFilterColor) {
-        filterProducts = filterProducts.filter(product => product.colors && product.colors.some(color => color.value === this.productFilterColor))
-      }
-      return filterProducts
-    },
-
-    goods () {
-      const offset = (this.page - 1) * this.NumberProductsPerPage
-      return this.getFilterProducts.slice(offset, offset + this.NumberProductsPerPage)
-    },
-    getAllProduct () {
-      return this.getFilterProducts.length
+    currentPage () {
+      return routerPage[this.page] || 'NotFoundPage'
     }
-
   }
 }
 </script>
---
+++ src/page/MainPage.vue
@@ -0,0 +1,83 @@
+<template>
+  <main class="content container">
+    <div class="content__top content__top--catalog">
+      <h1 class="content__title">
+        Каталог
+      </h1>
+      <span class="content__info">
+        {{getAllProduct}} товара
+      </span>
+    </div>
+
+    <div class="content__catalog">
+      <ProductFilter  :price-to.sync="productPriceTo" :price-from.sync="productPriceFrom"  :category-id.sync="productFilterId"
+      :category-color.sync="productFilterColor" :category-memory.sync="productFilterMamory"/>
+      <section class="catalog">
+        <ProductList :productList="goods"/>
+        <BasePagination :page.sync="page" :all-products="getAllProduct" :per-products="NumberProductsPerPage"/>
+      </section>
+    </div>
+  </main>
+
+</template>
+
+<script>
+import goods from '@/data/goods'
+import ProductList from '@/components/ProductList.vue'
+import BasePagination from '@/components/BasePagination.vue'
+import ProductFilter from '@/components/ProductFilter.vue'
+
+export default {
+  components: { ProductList, BasePagination, ProductFilter },
+
+  data () {
+    return {
+      productPriceFrom: 0,
+      productPriceTo: 0,
+      productFilterId: 0,
+      productFilterColor: '',
+      productFilterMamory: [],
+
+      page: 1,
+      NumberProductsPerPage: 4
+
+    }
+  },
+  computed: {
+    getFilterProducts () {
+      let filterProducts = goods
+
+      if (this.productPriceFrom > 0) {
+        filterProducts = filterProducts.filter(product => product.priceGoods > this.productPriceFrom)
+      }
+
+      if (this.productPriceTo > 0) {
+        filterProducts = filterProducts.filter(product => product.priceGoods < this.productPriceTo)
+      }
+      if (this.productFilterId) {
+        filterProducts = filterProducts.filter(product => product.goodsId === this.productFilterId)
+      }
+      if (this.productFilterMamory > 0) {
+        let allProductMemory
+        for (let i = 0; i < this.productFilterMamory.length; i++) {
+          allProductMemory = this.productFilterMamory[i]
+        }
+        filterProducts = filterProducts.filter(product => product.memory && product.memory.every(color => color.value === allProductMemory))
+      }
+      if (this.productFilterColor) {
+        filterProducts = filterProducts.filter(product => product.colors && product.colors.some(color => color.value === this.productFilterColor))
+      }
+      return filterProducts
+    },
+
+    goods () {
+      const offset = (this.page - 1) * this.NumberProductsPerPage
+      return this.getFilterProducts.slice(offset, offset + this.NumberProductsPerPage)
+    },
+    getAllProduct () {
+      return this.getFilterProducts.length
+    }
+
+  }
+}
+</script>
