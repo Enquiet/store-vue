6c4ee89be9b2d2df3430262998073dafb5e4d614
Enquiet <44980432+Enquiet@users.noreply.github.com>
2020-10-22 15:09:41 +0300

    оптемизировал компанент formcounter

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -13,21 +13,7 @@
       Артикул: {{itemProduct.product.id}}
     </span>
 
-    <div class="product__counter form__counter">
-      <button type="button" aria-label="Убрать один товар" :disabled="this.itemProduct.amount === 1" @click.prevent="(amount--)">
-        <svg width="10" height="10" fill="currentColor">
-          <use xlink:href="#icon-minus"></use>
-        </svg>
-      </button>
-
-      <input type="text" v-model.number="amount" name="count">
-
-      <button type="button" aria-label="Добавить один товар" @click.prevent="(amount++)">
-        <svg width="10" height="10" fill="currentColor">
-          <use xlink:href="#icon-plus"></use>
-        </svg>
-      </button>
-    </div>
+    <FormCounter :product-amaunt.sync="amount"/>
 
     <b class="product__price">
       {{ (itemProduct.amount * itemProduct.product.priceGoods) |  numberFormat}}
@@ -43,8 +29,12 @@
 <script>
 import numberFormat from '@/helpers/numberFormat'
 import { mapMutations } from 'vuex'
+import FormCounter from '@/components/FormCounter.vue'
 
 export default {
+  components: {
+    FormCounter
+  },
   filters: {
     numberFormat
   },
